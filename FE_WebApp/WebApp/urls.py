ec072613ea97a66fda57f4df4829012663c51a9d
moustafamahdy <moustafa.cena1997@gmail.com>
2021-06-13 05:50:19 +0800

    final commit

--- FE_WebApp/WebApp/urls.py
+++ FE_WebApp/WebApp/urls.py
@@ -6,7 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('Employees/', views.EmployeesList.as_view(), name="employees"),
     path('Employees/<int:pk>/', views.Employee.as_view(), name="employeedetail"),
-    path('create/', views.CreateEmployee.as_view(), name="create")
-    # path('sasa/', views.EmployeesList.sasa, name="employeelist")
+    path('create/', views.CreateEmployee.as_view(), name="create"),
+    path('Employees/<int:pk>/delete/', views.DeleteEmployee.as_view(), name="delete")
 
 ]
--- FE_WebApp/WebApp/views.py
+++ FE_WebApp/WebApp/views.py
@@ -1,24 +1,20 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
-from django import template
-from rest_framework.serializers import LIST_SERIALIZER_KWARGS
+from rest_framework.reverse import reverse
 from .models import Employees
 from .serializers import EmployeesSerializer
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.renderers import HTMLFormRenderer, TemplateHTMLRenderer
 from django.http import Http404
-from rest_framework import mixins, generics,viewsets
+from rest_framework import mixins, generics
 
 
 class EmployeesList(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "employees/EmployeesList.html"
-    # queryset = Employees.objects.all()
-    # serializer_class = EmployeesSerializer
 
     
     def get(self, request):
@@ -28,45 +24,16 @@ class EmployeesList(APIView):
             "employees": queryset
         })
 
-    # @LIST_SERIALIZER_KWARGS
-    
-    # def blank_form(self, request, *args, **kwargs):
-    #     # employee = get_object_or_404(Employees, pk= pk)
-    #     # self.object = self.g
-        
-    #     serilaizer = EmployeesSerializer()
-        
-
-
-    #     if not serilaizer.is_valid():
-    #         return Response(serilaizer.errors, status=status.HTTP_400_BAD_REQUEST) 
-
-    #     serilaizer.save()
-    #     return Response({
-    #             # "serializer": serilaizer
-    #             # # "employee": employee
-    #         }, status= status.HTTP_201_CREATED)
-        
 
 
 class CreateEmployee(mixins.CreateModelMixin, generics.GenericAPIView):
-    # queryset = Employees.objects.all()
-    # serializer_class = EmployeesSerializer
-    # template_name = 'employees/CreateEmployee.html'
-    # renderer_classes = [TemplateHTMLRenderer]
-
-    # def blank_form(self, request, *args):
-    #     serializer = EmployeesSerializer()
-    #     return Response({
-    #         "serializer": serializer
-    #     })
 
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
 
     def post(self, request):
-
-        return self.create(request)
+        self.create(request)
+        return HttpResponseRedirect(reverse('employees'))
     
 
 
@@ -91,98 +58,21 @@ class Employee(APIView):
             return Response({
                 "serializer": serilaizer,
                  "employee": employee
-            })
+            }, status= status.HTTP_202_ACCEPTED)
 
         serilaizer.save()
         return redirect("employees")
-# class EmployeesList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 
-#     queryset = Employees.objects.all()
-#     serializer_class = EmployeesSerializer
+class DeleteEmployee(APIView):
 
-#     def get(self, request):
-#         return self.list(request)
-#         # return self.list(request), render(request, "employees/EmployeesList.html", {
-#         #     "employees": Employees.objects.all()
-#         # })
-        
-#     def sasa(self, request):
-        
-#         ListOfEmployees = self.list(request)
-#         employees = Employees.objects.all()
-#         serializer = EmployeesSerializer(employees, many= True)
-#         return render(request, "employees/EmployeesList.html", {
+    def  get_Employee(self, pk):
+        try:
+            return Employees.objects.get(pk= pk)
+        except Employees.DoesNotExist:
+            raise Http404
 
-#             "employees": serializer.data
-            
+    def delete(self, request, pk, *args):
+        employee = self.get_Employee(pk)
+        employee.delete()
+        return HttpResponseRedirect(reverse('employees'))
 
-#         })
-
-#     def post(self, request):
-#         return self.create(request)
-
-
-# class Employee(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-
-#     queryset = Employees.objects.all()
-#     serializer_class = EmployeesSerializer
-
-#     def get(self, request, pk):
-
-#         return self.retrieve(request)
-#     def put(self, request, pk):
-
-#         return self.update(request)
-
-#     def delete(self, request, pk):
-        
-#         return self.destroy(request)
-
-
-
-
-
-
-
-# @api_view(['GET', 'POST'])
-
-# def EmployeesList(request):
-#     if request.method == 'GET':
-#         Employees_List = Employees.objects.all() # to get list of Employees
-#         serializer = EmployeesSerializer(Employees_List, many=True) # to serialize the Employee list
-#         return Response(serializer.data) # to return the Data to the user in Json format 
-
-#     if request.method == 'POST':
-#         # to give the data to the serializer
-#         serializer = EmployeesSerializer(data = request.data)
-#         # to check if all the required field exist 
-#         if serializer.is_valid():
-#             serializer.save()
-#             # to return the data to the user and tell him that the object is successfully created
-#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
-#         else:
-#             # return error for the user which means that the request is not valid
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def Employee (request, id):
-#     try:
-#         employee = Employees.objects.get(pk = id)
-#     except Employees.DoesNotExist:
-#         return Response(status = 404)
-
-#     if request.method == 'GET':
-#         serializer = EmployeesSerializer(employee)
-#         return Response(serializer.data)
-
-#     if request.method == 'PUT':
-#         serializer = EmployeesSerializer(employee, data= request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-#     if request.method == 'DELETE':
-#         employee.delete()
-#         return Response(status = status.HTTP_204_NO_CONTENT)
